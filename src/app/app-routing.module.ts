65a25f22bce20fadf2e3130da6a3e0d500238eb0
Razzakkhal <[khalfallah.razzak@gmail.com](mailto:khalfallah.razzak@gmail.com)>
2023-05-26 13:37:34 +0200

    quete finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'admin', loadChildren : () => import('./modules/adminModule/admin.module').then(m => m.AdminModule)},
+  {path : 'account', loadChildren : () => import('./modules/accountModule/account.module').then(m => m.AccountModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
